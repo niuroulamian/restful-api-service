2ca28ca7697c98cbef55223662235f2ed505f1bd
lixuan <janejia1019@gmail.com>
2024-07-02 13:32:10 +0200

    add main

---
+++ cmd/main.go
@@ -0,0 +1,99 @@
+// Package main implements external-api service command
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"go.uber.org/zap"
+
+	"github.com/niuroulamian/restful-api-service/config"
+	"github.com/niuroulamian/restful-api-service/internal/app"
+)
+
+var version string
+var rootCmd = &cobra.Command{
+	Use:               "service",
+	Short:             "template for go service",
+	Version:           version,
+	PersistentPreRunE: initConfig,
+	RunE:              run,
+}
+
+func main() {
+	rootCmd.PersistentFlags().Int("log-level", 4, "debug=5, info=4, error=2, fatal=1, panic=0")
+
+	if err := viper.BindPFlag("general.log_level", rootCmd.PersistentFlags().Lookup("log-level")); err != nil {
+		zap.S().Fatal(err)
+	}
+	if err := rootCmd.Execute(); err != nil {
+		zap.S().With(zap.Error(err)).Error("service exited with an error")
+		os.Exit(-1)
+	}
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	var cfg app.Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return fmt.Errorf("couldn't unmarshal config file: %v", err)
+	}
+	cfg.Version = version
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+	logger, err := mlog.New(
+		cfg.Logging,
+		mlog.WithService("service"),
+		mlog.WithVersion(version))
+	if err != nil {
+		return fmt.Errorf("couldn't initialize logger: %v", err)
+	}
+	ctx = mlog.ToContext(ctx, logger)
+
+	logger.Info("starting service")
+	a := app.New(cfg)
+	a.Start(ctx)
+
+	err = prompublisher.New(cfg.Prometheus).Start(ctx)
+	if err != nil {
+		return fmt.Errorf("prometheus start failed %v", err)
+	}
+	<-a.Done()
+	return fmt.Errorf("application has exited")
+}
+
+func initConfig(cmd *cobra.Command, args []string) error {
+	// load /etc/service/service.toml if exists
+	if _, err := os.Stat("/etc/service/service.toml"); err == nil {
+		// config file exists, load file's content
+		zap.S().Info("load configuration from /etc/service/service.toml")
+		viper.SetConfigFile("/etc/service/service.toml")
+		if err := viper.ReadInConfig(); err != nil {
+			return fmt.Errorf("couldn't read config file: %v", err)
+		}
+		return nil
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	// file /etc/service/service.toml does not exist,
+	// generate configuration content from service.tmpl
+	viper.SetEnvPrefix("")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	buff := bytes.NewBuffer(nil)
+	err := config.GetConfig(buff)
+	if err != nil {
+		return fmt.Errorf("couldn't get config: %v", err)
+	}
+	return viper.ReadConfig(buff)
+}
